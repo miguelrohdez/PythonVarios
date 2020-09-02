e3900ea50343744859a29a9415935c664f6fe977
Miguel Alejandro Rojas Hernández <nomeacuerdox4@gmail.com>
2020-09-02 02:49:46 -0500

    Update Piramide.py
    Descripcion

--- Piramide.py
+++ Piramide.py
@@ -1,3 +1,9 @@
+'''Lee la cantidad de bloques que tienen los constructores, y generar la altura de la pirámide que se puede construir utilizando estos bloques.
+La altura se mide por el número de capas completas: si los constructores 
+no tienen la cantidad suficiente de bloques y no pueden completar la siguiente capa, terminan su trabajo inmediatamente.
+Nota: La altura se mide por el número de capas completas: si los constructores no tienen la cantidad suficiente de bloques y
+no pueden completar la siguiente capa, terminan su trabajo inmediatamente.
+'''
 bloques = int(input("Ingrese el número de bloques:"))
 altura=0
 edo = 1
